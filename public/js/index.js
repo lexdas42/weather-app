6a0e8d9c8b709b66e95a8e7456255c57004d982a
Adelson Alexandro <adelson4448@gmail.com>
2020-12-18 07:34:17 -0800

    setup thing

--- public/js/index.js
+++ public/js/index.js
@@ -25,7 +25,7 @@ const location = search.value
 msg1.textContent = 'loading....'
 msg2.textContent = ''
 
-fetch('http://localhost:3000/weather?address='+ location).then((response) => {
+fetch('/weather?address='+ location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             msg1.textContent = data.error
